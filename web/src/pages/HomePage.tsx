8738bf01062601715661e4f0d1f8af3f1ced7383
Tim Adair <tymmo.t@gmail.com>
2025-07-11 18:23:53 -0600

    Add Adobe React Spectrum, routing to hello world page. Light/Dark from Provider.

---
+++ web/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Heading, View } from '@adobe/react-spectrum';
+
+export function HomePage() {
+  return (
+    <View padding="size-1000">
+      <Heading level={1}>Hello World!</Heading>
+      <p>Welcome to the Roman Numeral Converter</p>
+    </View>
+  );
+} 
